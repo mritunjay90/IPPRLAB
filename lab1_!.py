05f9e94ffacdcc855c5bfcd4857885d81c63ddd7
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-05-06 08:09:47 +0545

    Mritunjay Shah

---
+++ lab1_!.py
@@ -0,0 +1,8 @@
+import cv2
+from matplotlib import pyplot as plt
+img = cv2.imread('a.jpg',0)
+plt.subplot(121),plt.imshow(img)
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img,cmap = 'gray')
+plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
+plt.show()
