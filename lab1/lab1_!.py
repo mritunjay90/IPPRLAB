7cf8f7fc19aba07b052450393400aafc14673914
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-05-27 07:38:47 +0545

    lab2

--- lab1/lab1_!.py
+++ lab1/lab1_!.py
@@ -1,6 +1,6 @@
 import cv2
 from matplotlib import pyplot as plt
-img = cv2.imread('a.jpg',0)
+img = cv2.imread('h.jpg',0)
 plt.subplot(121),plt.imshow(img)
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(img,cmap = 'gray')
